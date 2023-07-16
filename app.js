22abf9d68b08b80850b7f6cd00d3224cc946b15b
drissakande <drissakande@gmail.com>
2023-07-16 09:15:38 -0400

    Move the position of createAnOrder function from the top the the middle

--- app.js
+++ app.js
@@ -25,6 +25,76 @@ const ProductName = document.getElementById("product-name");
 orderForm.style.visibility = "hidden";
 
 // Funsction to Create New Orders
+
+
+
+
+// Get request to render the products
+fetch(" http://localhost:3000/products")
+.then(response =>response.json())
+.then(products=>{
+    console.log(products);
+    products.forEach(product=>{
+        const eachProductContainer = document.createElement("div");
+        const productName = document.createElement("h5");
+        const productImage = document.createElement("img");
+        const productDescription = document.createElement("p");
+        const price = document.createElement("p");
+        const productsContainer = document.getElementById("products");
+
+        eachProductContainer.setAttribute("class", "eachProdcut")
+
+
+        productImage.setAttribute("src", product.image);
+        productName.textContent = product.name;
+        productDescription.textContent = product.description;
+        price.textContent = product.price;
+        eachProductContainer.appendChild(productImage);
+        eachProductContainer.appendChild(productName);
+        eachProductContainer.appendChild(productDescription);
+        eachProductContainer.appendChild(price);
+        productsContainer.appendChild(eachProductContainer);
+
+        eachProductContainer.addEventListener("click", (event)=>{
+            imageInfo.innerHTML = "";
+            const imageDescription = document.createElement("p");
+            const imageDescriptionName = document.createElement("h5");
+            const imageDescriptionPrice = document.createElement("p");
+            const img = document.createElement("img");
+            const btn1 = document.createElement("button");
+            const btn2 = document.createElement("button");
+
+        img.setAttribute("src", product.image);
+        imageDescription.textContent = product.description;
+        imageDescriptionName.textContent = product.name;
+        imageDescriptionPrice.textContent = product.price;
+        btn1.textContent = "Add to your car";
+        btn2.textContent = "Buy now.";
+        btn1.setAttribute("class", "bttn btn btn-success");
+        btn2.setAttribute("class", "bttn btn btn-success");
+    
+        imageInfo.appendChild(img);
+        imageInfo.appendChild(imageDescriptionName)
+        imageInfo.appendChild(imageDescription);
+        imageInfo.appendChild(imageDescriptionPrice)
+        imageInfo.appendChild(btn1);
+        imageInfo.appendChild(btn2);
+    
+        btn1.addEventListener("click", (event)=>{
+            alert("Added to your Cart")
+        });
+        btn2.addEventListener("click", ()=>{
+            orderForm.style.visibility = "visible";
+            form.style.display = "none";    
+        });
+   
+    });
+
+    })
+    
+});
+
+
 function createAnOrder(order){
     const firstName = document.createElement("p");
         const lastName = document.createElement("p");
@@ -81,75 +151,6 @@ function createAnOrder(order){
                
             })
 }
-
-
-
-// Get request to render the products
-fetch(" http://localhost:3000/products")
-.then(response =>response.json())
-.then(products=>{
-    console.log(products);
-    products.forEach(product=>{
-        const eachProductContainer = document.createElement("div");
-        const productName = document.createElement("h5");
-        const productImage = document.createElement("img");
-        const productDescription = document.createElement("p");
-        const price = document.createElement("p");
-        const productsContainer = document.getElementById("products");
-
-        eachProductContainer.setAttribute("class", "eachProdcut")
-
-
-        productImage.setAttribute("src", product.image);
-        productName.textContent = product.name;
-        productDescription.textContent = product.description;
-        price.textContent = product.price;
-        eachProductContainer.appendChild(productImage);
-        eachProductContainer.appendChild(productName);
-        eachProductContainer.appendChild(productDescription);
-        eachProductContainer.appendChild(price);
-        productsContainer.appendChild(eachProductContainer);
-
-        eachProductContainer.addEventListener("click", (event)=>{
-
-            imageInfo.innerHTML = "";
-    
-            const imageDescription = document.createElement("p");
-            const imageDescriptionName = document.createElement("h5");
-            const imageDescriptionPrice = document.createElement("p");
-            const img = document.createElement("img");
-            const btn1 = document.createElement("button");
-            const btn2 = document.createElement("button");
-
-        img.setAttribute("src", product.image);
-        imageDescription.textContent = product.description;
-        imageDescriptionName.textContent = product.name;
-        imageDescriptionPrice.textContent = product.price;
-        btn1.textContent = "Add to your car";
-        btn2.textContent = "Buy now.";
-        btn1.setAttribute("class", "bttn btn btn-success");
-        btn2.setAttribute("class", "bttn btn btn-success");
-    
-        imageInfo.appendChild(img);
-        imageInfo.appendChild(imageDescriptionName)
-        imageInfo.appendChild(imageDescription);
-        imageInfo.appendChild(imageDescriptionPrice)
-        imageInfo.appendChild(btn1);
-        imageInfo.appendChild(btn2);
-    
-        btn1.addEventListener("click", (event)=>{
-            alert("Added to your Cart")
-        });
-        btn2.addEventListener("click", ()=>{
-            orderForm.style.visibility = "visible";
-            form.style.display = "none";    
-        });
-   
-    });
-
-    })
-    
-});
 
 // Get Resquest for Orders
 fetch("http://localhost:3000/orders")
